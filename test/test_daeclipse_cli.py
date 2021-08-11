9508b73f20f405e58a84b29303adbb40fc91aa04
Kathryn DiPippo <kathryn.dipippo@acquia.com>
2021-08-11 15:13:15 -0400

    🔧 Add PyPI GitHub Action, remove unrelated docs/ folder, update version

--- test/test_daeclipse_cli.py
+++ test/test_daeclipse_cli.py
@@ -1,8 +1,8 @@
 """Test to cover daeclipse library."""
 
-from daeclipse import __version__
+from daeclipse_cli import __version__
 
 
 def test_version():
     """Test daeclipse library version."""
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.0.1'
